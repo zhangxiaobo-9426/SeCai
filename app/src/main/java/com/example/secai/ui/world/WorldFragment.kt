199f0bee4fa1bdb8181890cf10ba8dcc1702b5fd
15507 <1550751448@qq.com>
2021-01-25 17:48:01 +0800

    worldFragment
    在worldFragment中加入articleFragment和videoFragment，Viewpage2使左右滑动

--- app/src/main/java/com/example/secai/ui/world/WorldFragment.kt
+++ app/src/main/java/com/example/secai/ui/world/WorldFragment.kt
@@ -6,7 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.secai.R
+import com.example.secai.ui.world.article.ArticleFragment
+import com.example.secai.ui.world.video.VideoFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_world.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +27,25 @@ class WorldFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_world, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        world_viewPager2.apply {
+            adapter = object :FragmentStateAdapter(this@WorldFragment){
+            override fun getItemCount() =2
 
+            override fun createFragment(position: Int) =
+                when(position){
+                    0 ->ArticleFragment()
+                    else -> VideoFragment()
+                    }
+                }
+            setCurrentItem(0,false)
+            }
+        TabLayoutMediator(world_tabLayout,world_viewPager2){ tab: TabLayout.Tab, i: Int ->
+            tab.text = when(i){
+                0 -> "文章"
+                else ->"视频"
+            }
+        }.attach()
+        }
 }
