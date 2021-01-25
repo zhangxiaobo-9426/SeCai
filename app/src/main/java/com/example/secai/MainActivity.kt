ffe3bec4c0b20ba693e85604597a0347ece778a0
15507 <1550751448@qq.com>
2021-01-25 16:08:21 +0800

    BottomNavigation
    底部导航

--- app/src/main/java/com/example/secai/MainActivity.kt
+++ app/src/main/java/com/example/secai/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.secai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView:BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        NavigationUI.setupWithNavController(navView,navController)
     }
 }
